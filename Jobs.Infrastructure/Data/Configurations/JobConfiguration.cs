cbcef36c0717b673498ba465f8fe1815f1edd607
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2025-12-16 17:33:47 +0200

    edit job configuration

--- Jobs.Infrastructure/Data/Configurations/JobConfiguration.cs
+++ Jobs.Infrastructure/Data/Configurations/JobConfiguration.cs
@@ -12,32 +12,70 @@ namespace Jobs.Infrastructure.Data.Configurations
 			builder.ToTable("Jobs");
 
 			builder.HasKey(j => j.Id);
-			builder.Property(j => j.Id).ValueGeneratedNever();
+			builder.Property(j => j.Id)
+				   .ValueGeneratedNever();
 
 			builder.Property(j => j.Title)
-				.HasMaxLength(300)
-				.IsRequired();
+				   .HasMaxLength(200)
+				   .IsRequired();
 
 			builder.Property(j => j.Description)
-				.HasMaxLength(8000);
+				   .HasMaxLength(4000)
+				   .IsRequired();
 
-			builder.OwnsOne(j => j.Salary, s =>
+			builder.Property(j => j.Requirements)
+				   .HasMaxLength(4000);
+
+			builder.Property(j => j.ExperienceLevel)
+				   .IsRequired();
+
+			builder.Property(j => j.IsPublished)
+				   .HasDefaultValue(false);
+
+			builder.Property(j => j.ExpirationDate);
+
+			builder.HasOne(j => j.Company)
+				   .WithMany("_jobs")
+				   .HasForeignKey(j => j.CompanyId)
+				   .OnDelete(DeleteBehavior.Cascade);
+			builder.Navigation("_jobs")
+				   .UsePropertyAccessMode(PropertyAccessMode.Field);
+
+			builder.HasOne(j => j.Hr)
+				   .WithMany()
+				   .HasForeignKey(j => j.HrId)
+				   .OnDelete(DeleteBehavior.Restrict);
+
+			builder.OwnsOne(j => j.Salary, salary =>
 			{
-				s.Property(p => p.Min).HasColumnName("SalaryMin");
-				s.Property(p => p.Max).HasColumnName("SalaryMax");
+				salary.Property(s => s.Min)
+					  .HasColumnName("SalaryMin")
+					  .HasMaxLength(10);
+				salary.Property(s => s.Max)
+					  .HasColumnName("SalaryMax")
+					  .HasMaxLength(10);
+				salary.WithOwner();
 			});
 
-			builder.Property(j => j.IsPublished).HasDefaultValue(false);
-
-			builder.HasOne<Company>()
-				.WithMany()
-				.HasForeignKey(j => j.CompanyId)
-				.OnDelete(DeleteBehavior.Restrict);
-
-			builder.HasQueryFilter(p => !p.IsDeleted);
+			builder.HasMany<JobSkill>("_requiredSkills")
+				   .WithOne(js => js.Job)
+				   .HasForeignKey(js => js.JobId)
+				   .OnDelete(DeleteBehavior.Cascade);
+			builder.Navigation("_requiredSkills")
+				   .UsePropertyAccessMode(PropertyAccessMode.Field);
 
 
-			// required skills as separate table configured in JobSkillConfiguration
+			builder.Property(j => j.IsDeleted)
+				   .HasDefaultValue(false);
+
+			builder.Property(j => j.DeletedAt);
+
+			builder.HasQueryFilter(j => !j.IsDeleted);
+
+			// ===== Indexes =====
+			builder.HasIndex(j => j.CompanyId);
+			builder.HasIndex(j => j.IsPublished);
+			builder.HasIndex(j => j.ExpirationDate);
 		}
 	}
 }
