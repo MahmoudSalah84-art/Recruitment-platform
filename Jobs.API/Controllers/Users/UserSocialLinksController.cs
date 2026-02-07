7996fbdb575c2fe066b604b367571ca3fa5c877b
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2026-02-07 20:08:23 +0200

    end point of social link

--- Jobs.API/Controllers/Users/UserSocialLinksController.cs
+++ Jobs.API/Controllers/Users/UserSocialLinksController.cs
@@ -1,31 +1,51 @@
-﻿using Jobs.Domain.Entities;
-using MediatR;
+﻿using Jobs.API.Controllers.Abstractions;
+using Jobs.Application.Features.Users.Commands.DeleteSocialLink;
+using Jobs.Application.Features.Users.Commands.UpdateSinglePlatform;
+using Jobs.Application.Features.Users.Commands.UpdateSocialLinks;
+using Jobs.Application.Features.Users.Queries.GetMySocialLinks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
-using System;
 
 namespace Jobs.API.Controllers.Users
 {
-	public class UserSocialLinksController : ControllerBase
+	[Route("api/users/me/social-links")]
+	public class UserSocialLinksController : ApiController
 	{
-		private readonly IMediator _mediator;
-		public UserSocialLinksController(IMediator mediator) => _mediator = mediator;
-
-		[HttpPut]
-		public async Task<IActionResult> Update([FromBody] UpdateSocialLinksCommand command)
+		// GET /api/users/me/social-links
+		[HttpGet]
+		public async Task<IActionResult> Get()
 		{
-			var result = await _mediator.Send(command);
-			return result.IsSuccess ? Ok(result) : BadRequest(result.Error);
+			var result = await Sender.Send(new GetMySocialLinksQuery());
+			return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+		}
+
+		// POST /api/users/me/social-links
+		[HttpPost]
+		public async Task<IActionResult> CreateOrUpdate(
+			[FromBody] UpdateMySocialLinksCommand command)
+		{
+			var result = await Sender.Send(command);
+			return result.IsSuccess ? Ok() : BadRequest(result.Error);
+		}
+
+		// PUT /api/users/me/social-links/{platform}
+		[HttpPut("{platform}")]
+		public async Task<IActionResult> Update( string platform, [FromBody] UpdateSingleSocialLinkCommand command)
+		{
+			if (platform != command.Platform)
+				return BadRequest("platform in URL does not match platform in body.");
+
+			var result = await Sender.Send(command);
+			return result.IsSuccess ? NoContent() : BadRequest(result.Error);
+		}
+
+		// DELETE /api/users/me/social-links/{platform}
+		[HttpDelete("{platform}")]
+		public async Task<IActionResult> Delete(string platform)
+		{
+			var result = await Sender.Send( new DeleteSocialLinkCommand(platform));
+
+			return result.IsSuccess ? NoContent() : BadRequest(result.Error);
 		}
 	}
+
 }
-
-//Method, Route, Description
-//GET,/api/social-links, جلب روابط التواصل الاجتماعي الخاصة بالمستخدم.
-//PUT,/api/social-links, تحديث الروابط (عادة تكون مجموعة روابط واحدة يتم تحديثها معاً).
-
-
-//GET / api / users / me / social - links
-//POST / api / users / me / social - links
-//PUT / api / users / me / social - links /{ id}
-//DELETE / api / users / me / social - links /{ id}
