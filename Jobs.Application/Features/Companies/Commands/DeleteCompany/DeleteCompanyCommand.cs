af9180ea65259cf114a117cc42778087ca59f2be
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2026-03-03 09:58:51 +0200

    remove old folder

--- Jobs.Application/Features/Companies/Commands/DeleteCompany/DeleteCompanyCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Jobs.Application.Abstractions.Messaging;
- 
-namespace Jobs.Application.Features.Companies.Commands.DeleteCompany
-{
-	public record DeleteCompanyCommand(Guid CompanyId) : ICommand;
-}
